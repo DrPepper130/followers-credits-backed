c1c8075c46ce3317825c3253e39410fa247b2679
DrPepper130 <lucasmaraman@icloud.com>
2026-04-07 19:41:31 -0700

    Update server.js

--- server.js
+++ server.js
@@ -351,3 +351,9 @@ app.post("/api/orders/create", async (req, res) => {
     })
   }
 })
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`)
+})
